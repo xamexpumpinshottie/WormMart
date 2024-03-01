265a144c813fe8949cbd194c290086963832b510
xamexpumpinshottie <157251442+xamexpumpinshottie@users.noreply.github.com>
2024-03-01 23:24:35 +0300

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,9 +1,10 @@
+
 <?php
-$mysql_hostname = "localhost";
-$mysql_user ="root";
-$mysql_password ="123";
-$mysql_database ="blackmart_data";
-$bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("oops !Could not connect database");
-mysql_select_db($mysql_database, $bd) or die("opps! database not connected");
+$mysql_hostname = "aws.connect.psdb.cloud";
+$mysql_user ="v7c8oo7cckn7wr5pkbyg";
+$mysql_password ="pscale_pw_QKUnNrmQrlgcd5lyhDWw8NY9LBt3ZmurVwnBHMEJviU";
+$mysql_database ="Products";
+$bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
+mysql_select_db($mysql_database, $bd) or die("database not connected");
 
 ?>
